60c6901bf5852f8e42ff44445b7c83afbf119c13
Poscat <poscat@mail.poscat.moe>
2020-06-03 12:01:06 +0800

    Add simple test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,61 @@
 module Main where
 
-main = pure ()
+import Control.Concurrent (forkIO)
+import Control.Concurrent.Chan.Unagi
+import Data.Text (Text)
+import Data.Text.Arbitrary
+import Polysemy hiding (run)
+import TDLib.Effect
+import TDLib.EventLoop
+import TDLib.Generated.FunArgs
+import TDLib.Generated.Functions
+import TDLib.Generated.Types hiding (Text)
+import TDLib.Types.Common
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+import Text.Pretty.Simple
+
+-- ** Helper functions
+
+mm ::
+  Testable a =>
+  InChan Update ->
+  PropertyM (Sem '[TDLib, Embed IO]) a ->
+  Property
+mm ichan = monadic (r ichan)
+
+r ichan m = (monadicIO . run . runM . runTDLibEventLoop 0.1 ichan) (setVerbosity Fatal >> m)
+
+updateConsumer :: OutChan Update -> IO ()
+updateConsumer chan = go
+  where
+    go = do
+      u <- readChan chan
+      go
+
+-- ** Main
+
+main :: IO ()
+main = do
+  (ichan, ochan) <- newChan
+  forkIO $ updateConsumer ochan
+  quickCheck $ squareInt ichan
+  quickCheck $ callEmpty ichan
+  quickCheck $ callString ichan
+
+-- ** Properties
+
+squareInt :: InChan Update -> I32 -> Property
+squareInt ichan i = mm ichan $ do
+  r <- run $ testSquareInt (TestSquareInt i)
+  assert $ Inr (TestInt $ i ^ 2) == r
+
+callEmpty :: InChan Update -> Property
+callEmpty ichan = mm ichan $ do
+  ok <- run $ testCallEmpty TestCallEmpty
+  assert $ Inr Ok == ok
+
+callString :: InChan Update -> Text -> Property
+callString ichan t = mm ichan $ do
+  r <- run $ testCallString (TestCallString t)
+  assert $ Inr (TestString t) == r
