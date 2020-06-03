d1b9d5e45f4b72e6e77abe1f0453df94adc446d7
Poscat <poscat@mail.poscat.moe>
2020-06-03 13:15:55 +0800

    Fix warnings

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,7 +4,6 @@ import Control.Concurrent (forkIO)
 import Control.Concurrent.Chan.Unagi
 import Data.ByteString (ByteString)
 import Data.ByteString.Base64.Type
-import Data.Text (Text)
 import Data.Text.Arbitrary
 import Polysemy hiding (run)
 import TDLib.Effect
@@ -15,7 +14,6 @@ import TDLib.Generated.Types hiding (Text)
 import TDLib.Types.Common
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
-import Text.Pretty.Simple
 
 -- ** Helper functions
 
@@ -24,15 +22,20 @@ mm ::
   InChan Update ->
   PropertyM (Sem '[TDLib, Embed IO]) a ->
   Property
-mm ichan = monadic (r ichan)
+mm ichan = monadic (rr ichan)
 
-r ichan m = (monadicIO . run . runM . runTDLibEventLoop 0.1 ichan) (setVerbosity Fatal >> m)
+rr ::
+  Testable a =>
+  InChan Update ->
+  Sem '[TDLib, Embed IO] a ->
+  Property
+rr ichan m = (monadicIO . run . runM . runTDLibEventLoop 0.1 ichan) (setVerbosity Fatal >> m)
 
 updateConsumer :: OutChan Update -> IO ()
 updateConsumer chan = go
   where
     go = do
-      u <- readChan chan
+      readChan chan
       go
 
 -- ** Main
@@ -54,7 +57,7 @@ main = do
 squareInt :: InChan Update -> I32 -> Property
 squareInt ichan i = mm ichan $ do
   r <- run $ testSquareInt (TestSquareInt i)
-  assert $ Inr (TestInt $ i ^ 2) == r
+  assert $ Inr (TestInt $ (i :: Int) ^ (2 :: Int)) == r
 
 callEmpty :: InChan Update -> Property
 callEmpty ichan = mm ichan $ do
